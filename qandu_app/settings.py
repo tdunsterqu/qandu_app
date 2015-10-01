808c57484e3c96bfc93e17d71a6978431d677325
tdunsterqu <tcdunster@quinnipiac.edu>
2015-10-01 19:12:06 +0000

    Add user authentication

--- qandu_app/settings.py
+++ qandu_app/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'registration',
+    'django.contrib.sites',
 
 )
 
@@ -115,3 +117,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(MAIN_DIR, 'static'),)
 STATIC_ROOT = 'staticfiles'
+
+LOGIN_URL = '/user/login'
+LOGIN_REDIRECT_URL ='/'
+SITE_ID = 1
