837f7aa6ea7fa4b8cfed5f54f7386b31af16a0fd
tdunsterqu <tcdunster@quinnipiac.edu>
2015-10-26 01:07:31 +0000

    Add user update view

--- core/views.py
+++ core/views.py
@@ -143,6 +143,21 @@ class UserDetailView(DetailView):
     context['answers'] = answers
     return context
 
+class UserUpdateView(UpdateView):
+  model = User
+  slug_field = 'username'
+  template_name = 'user/user_form.html'
+  fields = ['email', 'first_name', 'last_name']
+
+  def get_success_url(self):
+    return reverse('user_detail', args=[self.request.user.username])
+
+  def get_object(self, *args, **kwargs):
+    object = super(UserUpdateView, self).get_object(*args, **kwargs)
+    if object != self.request.user:
+      raise PermissionDenied()
+    return object
+
 
 
 
